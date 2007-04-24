b741f594871879598cdf49be577324acd14078dc
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-24 19:19:30 +0000

    User: Kay          Date: 25.04.07   Time: 15:54 (vss version: 1, vss user: Kay)

---
+++ SuRun/WinStaDesk.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ScreenSnap.h"
+
+// WindowStation Desktop Names:
+
+BOOL GetWinStaName(LPTSTR WinSta,DWORD ccWinSta);
+BOOL GetDesktopName(LPTSTR DeskName,DWORD ccDeskName);
+
+//This class creates a new desktop with a darkened blurred image of the 
+//current dektop as background.
+class CRunOnNewDeskTop
+{
+public:
+  CRunOnNewDeskTop(LPCTSTR DeskName,BOOL bCreateBkWnd);
+  ~CRunOnNewDeskTop();
+  bool IsValid();
+private:
+  void CleanUp();
+  bool    m_bOk;
+  HWINSTA m_hwinstaSave;
+  HWINSTA m_hwinstaUser;
+  HDESK   m_hdeskSave;
+  HDESK   m_hdeskUser;
+  HDESK   m_hDeskSwitch;
+  CBlurredScreen m_Screen;
+};
+
+void GrantAccessToWinstationAndDesktop(HANDLE htok,LPCTSTR WinSta,LPCTSTR Desk);
