215a71606398e09ab5e5a624d741209990df87d9
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-25 13:59:56 +0000

    User: Kay          Date: 25.04.07   Time: 15:54 (vss version: 1, vss user: Kay)

---
+++ SuRun/resource.h
@@ -0,0 +1,57 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by SuRun.rc
+//
+#define IDS_USAGE                       1
+#define IDS_ASKOK                       2
+#define IDS_APPNAME                     3
+#define IDS_NODESK                      4
+#define IDS_SERVICE_NAME                5
+#define IDS_ASKINSTALL                  6
+#define IDS_UNINSTALLADMIN              7
+#define IDS_INSTALLADMIN                8
+#define IDS_LOGONFAILED                 9
+#define IDS_NOADMIN                     10
+#define IDS_NOSUDOER                    11
+#define IDS_ASKSUDOER                   12
+#define IDS_BUILTINADMIN                14
+#define IDS_SUDOER_OK                   15
+#define IDS_SUDOER_ERR                  16
+#define IDS_LOGOFFON                    17
+#define IDD_LOGONDLG                    101
+#define IDI_ICON1                       103
+#define IDI_MAINICON                    103
+#define IDD_DIALOG1                     104
+#define IDI_SECURITY                    106
+#define IDD_SETUP                       107
+#define IDD_CURUSRLOGON                 108
+#define IDD_CURUSRACK                   109
+#define IDC_USER                        1000
+#define IDC_DLGQUESTION                 1001
+#define IDC_PASSWORD                    1002
+#define IDC_DOMAIN                      1003
+#define IDC_USERBITMAP                  1004
+#define IDC_WHTBK                       1005
+#define IDC_SECICON                     1006
+#define IDC_RADIO1                      1007
+#define IDC_RADIO2                      1008
+#define IDC_RADIO3                      1009
+#define IDC_ASKTIMEOUT                  1010
+#define IDC_BLURDESKTOP                 1011
+#define IDC_HINT                        1017
+#define IDC_HINTBK                      1018
+#define IDC_SECICON1                    1019
+#define IDI_SHIELD                      10605
+#define IDI_SHIELD2                     10606
+#define IDI_SHIELD1                     10607
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
