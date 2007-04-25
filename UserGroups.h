00091d58ea2cd4048effdedf342caa4d50bcb984
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-25 05:35:58 +0000

    User: Kay          Date: 25.04.07   Time: 15:54 (vss version: 1, vss user: Kay)

---
+++ UserGroups.h
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Most of this is heavily based on SuDown (http://sudown.sourceforge.net)
+//
+/////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#define SUDOERSGROUP  _T("sudoers")
+
+const DWORD UserGroups[]=
+{
+  DOMAIN_ALIAS_RID_ADMINS,
+  DOMAIN_ALIAS_RID_POWER_USERS,
+  DOMAIN_ALIAS_RID_USERS,
+  DOMAIN_ALIAS_RID_GUESTS
+};
+
+BOOL GetGroupName(DWORD Rid,LPWSTR Name,PDWORD cchName);
+
+DWORD AlterGroupMember(LPCWSTR Group,LPCWSTR DomainAndName, BOOL bAdd);
+DWORD AlterGroupMember(DWORD Rid,LPCWSTR DomainAndName, BOOL bAdd);
+
+BOOL IsInGroup(LPCWSTR Group,LPCWSTR DomainAndName);
+BOOL IsInGroup(DWORD Rid,LPCWSTR DomainAndName);
+BOOL IsInSudoers(LPCWSTR DomainAndName);
+
+BOOL IsBuiltInAdmin(LPCWSTR DomainAndName);
