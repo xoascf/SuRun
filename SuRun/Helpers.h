5365948730feb0e31ddabe48916bb6a6fd00fdf9
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-25 06:44:55 +0000

    User: Kay          Date: 25.04.07   Time: 15:54 (vss version: 1, vss user: Kay)

---
+++ SuRun/Helpers.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define countof(b) sizeof(b)/sizeof(b[0])
+#define zero(x) memset(&x,0,sizeof(x))
+
+//  Registry Helper
+BOOL RegDelVal(HKEY HK,LPCTSTR SubKey,LPCTSTR ValName);
+
+BOOL SetRegInt(HKEY HK,LPCTSTR SubKey,LPCTSTR ValName,DWORD Value);
+DWORD GetRegInt(HKEY HK,LPCTSTR SubKey,LPCTSTR ValName,DWORD Default);
+
+BOOL SetRegStr(HKEY HK,LPCTSTR SubKey,LPCTSTR ValName,LPCTSTR Value);
+BOOL GetRegStr(HKEY HK,LPCTSTR SubKey,LPCTSTR Val,LPTSTR Str,DWORD ccMax);
+
+BOOL RegEnum(HKEY HK,LPCTSTR SubKey,int Index,LPTSTR Str,DWORD ccMax);
+
+// Privilege stuff
+BOOL EnablePrivilege(HANDLE hToken,LPCTSTR name);
+BOOL EnablePrivilege(LPCTSTR name);
+
+//  AllowAccess
+void AllowAccess(HANDLE hObject);
+
+//  NetworkPathToUNCPath
+BOOL NetworkPathToUNCPath(LPTSTR path);
+
+//UserName:
+bool GetSIDUserName(PSID sid,LPTSTR Name);
+bool GetTokenUserName(HANDLE hUser,LPTSTR Name);
+bool GetProcessUserName(DWORD ProcessID,LPTSTR Name);
+
