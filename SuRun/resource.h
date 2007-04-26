0cd93aaaab83661c156a80042e49ed88259aa4ae
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-26 13:53:49 +0000

    User: Kay          Date: 26.04.07   Time: 15:09 (vss version: 5, vss user: Kay)

--- SuRun/resource.h
+++ SuRun/resource.h
@@ -25,6 +25,7 @@
 #define IDS_SURUNREPAIR                 21
 #define IDS_UNISTREBOOT                 22
 #define IDS_RUNFAILED                   23
+#define IDS_INSTALLOK                   24
 #define IDD_LOGONDLG                    101
 #define IDI_ICON1                       103
 #define IDI_MAINICON                    103
