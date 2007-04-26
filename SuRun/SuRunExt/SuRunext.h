da69ac4019e8133126a1d446e6e5811a6163a9c4
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-26 07:19:45 +0000

    User: Kay          Date: 26.04.07   Time:  8:23 (vss version: 5, vss user: Kay)

--- SuRun/SuRunExt/SuRunext.h
+++ SuRun/SuRunExt/SuRunext.h
@@ -4,6 +4,7 @@
 
 // {2C7B6088-5A77-4d48-BE43-30337DCA9A86}
 DEFINE_GUID(CLSID_ShellExtension,0x2c7b6088,0x5a77,0x4d48,0xbe,0x43,0x30,0x33,0x7d,0xca,0x9a,0x86);
+#define sGUID L"{2C7B6088-5A77-4d48-BE43-30337DCA9A86}"
 
 #pragma data_seg()
 
