245b8910c7626f3f4989a6936817f01038bdb6f4
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-04-30 13:35:46 +0000

    User: Kay          Date: 30.04.07   Time: 14:37 (vss version: 2, vss user: Kay)

--- SuRun/LogonDlg.h
+++ SuRun/LogonDlg.h
@@ -1,5 +1,7 @@
 #pragma once
 
+BOOL PasswordOK(LPCTSTR User,LPCTSTR Password);
+
 BOOL Logon(LPTSTR User,LPTSTR Password,int IDmsg,...);
 BOOL LogonAdmin(LPTSTR User,LPTSTR Password,int IDmsg,...);
 BOOL LogonCurrentUser(LPTSTR User,LPTSTR Password,int IDmsg,...);
