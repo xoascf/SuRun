0263eac16c9b0c7b3aad25aa1e86b3337232fcb7
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-01 11:44:05 +0000

    User: Kay          Date:  1.05.07   Time: 14:14 (vss version: 3, vss user: Kay)

--- Service.h
+++ Service.h
@@ -2,5 +2,20 @@
 
 #define ServicePipeName _T("\\\\.\\Pipe\\SuperUserRun")
 
+typedef struct
+{
+  DWORD CliProcessId;
+  DWORD SessionID;
+  TCHAR WinSta[MAX_PATH];
+  TCHAR Desk[MAX_PATH];
+  TCHAR UserName[CNLEN+DNLEN];
+  TCHAR cmdLine[4096];
+  TCHAR CurDir[MAX_PATH];
+  DWORD KillPID;
+}RUNDATA;
+
+extern RUNDATA g_RunData;
+extern TCHAR g_RunPwd[PWLEN];
+
 BOOL InstallService();
 BOOL DeleteService(BOOL bJustStop=FALSE);
