e3ba80d9b25f13c735bfdd7880269b4a6242780e
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-02 12:13:38 +0000

    (vss version: 8, vss user: Kay)

--- Service.h
+++ Service.h
@@ -28,7 +28,7 @@ typedef struct
   DWORD SessionID;
   TCHAR WinSta[MAX_PATH];
   TCHAR Desk[MAX_PATH];
-  TCHAR UserName[UNLEN];
+  TCHAR UserName[CNLEN+DNLEN];
   TCHAR cmdLine[4096];
   TCHAR CurDir[MAX_PATH];
   DWORD KillPID;
