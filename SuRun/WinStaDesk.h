c4a62b908638604540b4171818db097ff2748266
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-02 11:01:41 +0000

    User: Kay          Date:  2.05.07   Time: 13:29 (vss version: 4, vss user: Kay)

--- SuRun/WinStaDesk.h
+++ SuRun/WinStaDesk.h
@@ -1,3 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                   (c) Kay Bruns (http://kay-bruns.de), 2007
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Home of the CRunOnNewDeskTop class
+//
+// this class creates a new desktop in a given window station, captures the 
+// screen of the current desktop, switches to the new desktop and sets the 
+// captured screen as blurred backgroud.
+//
+// in the class destructor it swiches back to the users desktop
+//////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #include "ScreenSnap.h"
 
