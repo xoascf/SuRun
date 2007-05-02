623391ab615aa32fc1cba792e7543af718fcfef7
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-02 11:23:33 +0000

    User: Kay          Date:  2.05.07   Time: 13:29 (vss version: 3, vss user: Kay)

--- LogonDlg.h
+++ LogonDlg.h
@@ -1,5 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                   (c) Kay Bruns (http://kay-bruns.de), 2007
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Display various Dialogs to:
+// *Logon a Windows User
+// *Logon an Administrator
+// *Logon the current user
+// *Ask, if something should be started with administrative rights
+//////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+// Check is a password for a user is correct
 BOOL PasswordOK(LPCTSTR User,LPCTSTR Password);
 
 BOOL Logon(LPTSTR User,LPTSTR Password,int IDmsg,...);
