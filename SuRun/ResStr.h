18967b3f0b815a947dd8b14298969ff79cdf8b4a
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-02 11:21:08 +0000

    User: Kay          Date:  2.05.07   Time: 13:29 (vss version: 4, vss user: Kay)

--- SuRun/ResStr.h
+++ SuRun/ResStr.h
@@ -1,6 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                   (c) Kay Bruns (http://kay-bruns.de), 2007
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// 
+//Quick and dirty resource string class
+// 
+//////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-//Quick and dirty resource string class
 template<const int _S> class CResourceString
 {
 public:
