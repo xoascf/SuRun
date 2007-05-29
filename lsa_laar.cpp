6bf99e57339fa671a9d866b3094d4465c008fc7e
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-05-29 11:45:14 +0000

    User: Kay          Date: 29.05.07   Time: 21:10 (vss version: 2, vss user: Kay)

--- lsa_laar.cpp
+++ lsa_laar.cpp
@@ -1,8 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                   (c) Kay Bruns (http://kay-bruns.de), 2007
+//////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////
+// This code is based on 'MVPs.org's LsaAddAccountRights sample
+//    http://win32.mvps.org/lsa/lsa_leawur.cpp
+//////////////////////////////////////////////////////////////////////////////
+
 #define _WIN32_WINNT 0x500
 #define WINVER       0x500
 #include <windows.h>
 #include <ntsecapi.h>
-#include <stdio.h>
 #include <TCHAR.h>
 #include "lsa_laar.h"
 
