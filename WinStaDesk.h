91b2a9e5b8775f81c642170ac0fcd237267dd7f6
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-10-09 19:06:43 +0000

    User: Kay          Date:  9.10.07   Time: 21:13 (vss version: 5, vss user: Kay)

--- WinStaDesk.h
+++ WinStaDesk.h
@@ -48,3 +48,63 @@ private:
   HDESK   m_hDeskSwitch;
   CBlurredScreen m_Screen;
 };
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// CStayOnDeskTop
+//
+// There's no way to keep a thread in a different process from calling 
+// SwitchDesktop(). 
+// Even worse: Microsoft suggests using SwitchDeskTop(OpenDesktop("Default"))
+// to detect if a screen saver or logon screen is active. 
+// Apparently ShedHlp.exe from Acronis TrueImage 11 uses this trick. So I'm
+// forced to use CStayOnDeskTop to switch back to SuRuns desktop.
+//////////////////////////////////////////////////////////////////////////////
+
+class CStayOnDeskTop
+{
+public:
+  CStayOnDeskTop(LPCTSTR DeskName)
+  {
+    m_DeskName=_tcsdup(DeskName);
+    m_Thread=CreateThread(0,0,ThreadProc,this,0,0);
+  }
+  ~CStayOnDeskTop()
+  {
+    LPTSTR s=m_DeskName;
+    m_DeskName=0;
+    free(s);
+    WaitForSingleObject(m_Thread,INFINITE);
+  }
+  static DWORD WINAPI ThreadProc(void* p)
+  {
+    CStayOnDeskTop* t=(CStayOnDeskTop*)p;
+    SetThreadPriority(t->m_Thread,THREAD_PRIORITY_IDLE);
+    LPTSTR DeskName=_tcsdup(t->m_DeskName);
+    while (t->m_DeskName)
+    {
+      HDESK d=OpenDesktop(t->m_DeskName,0,FALSE,DESKTOP_SWITCHDESKTOP);
+      if (d!=0)
+      {
+        HDESK i=OpenInputDesktop(0,FALSE,DESKTOP_SWITCHDESKTOP);
+        if (i!=0)
+        {
+          TCHAR n[MAX_PATH]={0};
+          DWORD l=MAX_PATH;
+          if (GetUserObjectInformation(i,UOI_NAME,n,l,&l))
+            if (_tcscmp(n,DeskName))
+              SwitchDesktop(d);
+          CloseDesktop(i);
+        }
+        CloseDesktop(d);
+      }
+      Sleep(40);
+    }
+    free(DeskName);
+    t->m_Thread=0;
+    return 0;
+  }
+private:
+  LPTSTR m_DeskName;
+  HANDLE m_Thread;
+};
