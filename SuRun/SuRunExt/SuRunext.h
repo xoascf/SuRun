48075a9fd819cd8830bdec59ace342746e796720
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-20 21:59:11 +0000

    User: Kay          Date: 20.11.07   Time: 23:02 (vss version: 10, vss user: Kay)

--- SuRun/SuRunExt/SuRunext.h
+++ SuRun/SuRunExt/SuRunext.h
@@ -11,10 +11,6 @@ extern "C" __declspec(dllexport) void InstallShellExt();
 DEFINE_GUID(CLSID_ShellExtension,0x2c7b6088,0x5a77,0x4d48,0xbe,0x43,0x30,0x33,0x7d,0xca,0x9a,0x86);
 #define sGUID L"{2C7B6088-5A77-4d48-BE43-30337DCA9A86}"
 
-// {9F555256-BE01-41d2-B8AD-D268A5D89240}
-DEFINE_GUID(IID_IShellExecHook, 0x9f555256,0xbe01,0x41d2,0xb8,0xad,0xd2,0x68,0xa5,0xd8,0x92,0x40);
-#define sGUIDhk L"{9F555256-BE01-41d2-B8AD-D268A5D89240}"
-
 #pragma data_seg()
 
 // this class factory object creates context menu handlers for windows 32 shell
@@ -36,7 +32,7 @@ public:
 typedef CShellExtClassFactory *LPCSHELLEXTCLASSFACTORY;
 
 // this is the actual OLE Shell context menu handler
-class CShellExt : public IContextMenu, IShellExtInit
+class CShellExt : public IContextMenu, IShellExtInit, IShellExecuteHook
 {
 protected:
   ULONG	 m_cRef;
@@ -54,20 +50,8 @@ public:
   STDMETHODIMP GetCommandString(UINT_PTR, UINT, UINT FAR *, LPSTR, UINT);
   //IShellExtInit methods
   STDMETHODIMP Initialize(LPCITEMIDLIST, LPDATAOBJECT, HKEY);
-};
-
-class CShellExecHook : public IShellExecuteHook
-{
-protected:
-  ULONG	 m_cRef;
-public:
-  CShellExecHook();
-  ~CShellExecHook();
-  //IUnknown members
-  STDMETHODIMP QueryInterface(REFIID, LPVOID FAR *);
-  STDMETHODIMP_(ULONG) AddRef();
-  STDMETHODIMP_(ULONG) Release();
   //IShellExecuteHook methods
   STDMETHODIMP Execute(LPSHELLEXECUTEINFO pei);
 };
+
 typedef CShellExt *LPCSHELLEXT;
