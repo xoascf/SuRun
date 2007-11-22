ec587d08a81b105b6c9f5c7b50936e90a1beea09
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-22 10:38:33 +0000

    User: Kay          Date: 22.11.07   Time: 15:00 (vss version: 11, vss user: Kay)

--- SuRun/SuRunExt/SuRunext.h
+++ SuRun/SuRunExt/SuRunext.h
@@ -5,13 +5,14 @@
 extern "C" __declspec(dllexport) void RemoveShellExt();
 extern "C" __declspec(dllexport) void InstallShellExt();
 
-#pragma data_seg(".SHARDATA")
-
 // {2C7B6088-5A77-4d48-BE43-30337DCA9A86}
 DEFINE_GUID(CLSID_ShellExtension,0x2c7b6088,0x5a77,0x4d48,0xbe,0x43,0x30,0x33,0x7d,0xca,0x9a,0x86);
 #define sGUID L"{2C7B6088-5A77-4d48-BE43-30337DCA9A86}"
 
-#pragma data_seg()
+#define ControlAsAdmin  L"ControlAsAdmin"  //"Control Panel As Admin" on Desktop Menu
+#define RestartAsAdmin  L"RestartAsAdmin"  //"Restart As Admin" in System-Menu
+#define StartAsAdmin    L"StartAsAdmin"    //"Start As Admin" in System-Menu
+
 
 // this class factory object creates context menu handlers for windows 32 shell
 class CShellExtClassFactory : public IClassFactory
