0dc5211f3d0966d2f31a6c182b35ab69177a0809
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-22 20:39:40 +0000

    User: Kay          Date: 22.11.07   Time: 21:44 (vss version: 23, vss user: Kay)

--- SuRun/SuRunExt/SysMenuHook.cpp
+++ SuRun/SuRunExt/SysMenuHook.cpp
@@ -1,3 +1,17 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                   (c) Kay Bruns (http://kay-bruns.de), 2007
+//////////////////////////////////////////////////////////////////////////////
+
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
@@ -12,7 +26,11 @@
 
 #pragma comment(lib,"shlwapi")
 
+//////////////////////////////////////////////////////////////////////////////
+//
 // global data within shared data segment to allow sharing across instances
+//
+//////////////////////////////////////////////////////////////////////////////
 #pragma data_seg(".SHARDATA")
 
 HHOOK       g_hookShell = NULL;
