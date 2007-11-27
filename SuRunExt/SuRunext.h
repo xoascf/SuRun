2fb13da55208f5fc1a128967078790d1b1492a16
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-27 15:16:47 +0000

    User: Kay          Date: 27.11.07   Time: 16:28 (vss version: 18, vss user: Kay)

--- SuRunExt/SuRunext.h
+++ SuRunExt/SuRunext.h
@@ -22,10 +22,3 @@ extern "C" __declspec(dllexport) void InstallShellExt();
 // {2C7B6088-5A77-4d48-BE43-30337DCA9A86}
 DEFINE_GUID(CLSID_ShellExtension,0x2c7b6088,0x5a77,0x4d48,0xbe,0x43,0x30,0x33,0x7d,0xca,0x9a,0x86);
 #define sGUID L"{2C7B6088-5A77-4d48-BE43-30337DCA9A86}"
-
-//the following settings are stored in: HKCR\\CLSID\\sGUID
-#define ControlAsAdmin  L"ControlAsAdmin"  //"Control Panel As Admin" on Desktop Menu
-#define CmdHereAsAdmin  L"CmdHereAsAdmin"  //"Cmd here As Admin" on Folder Menu
-#define ExpHereAsAdmin  L"ExpHereAsAdmin"  //"Explorer here As Admin" on Folder Menu
-#define RestartAsAdmin  L"RestartAsAdmin"  //"Restart As Admin" in System-Menu
-#define StartAsAdmin    L"StartAsAdmin"    //"Start As Admin" in System-Menu
