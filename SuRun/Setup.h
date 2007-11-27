1a8ca09a1ab44160ebe81217c33fe76ed5739a1c
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-27 14:27:29 +0000

    User: Kay          Date: 27.11.07   Time: 16:28 (vss version: 5, vss user: Kay)

--- SuRun/Setup.h
+++ SuRun/Setup.h
@@ -14,28 +14,120 @@
 
 #pragma once
 
+//////////////////////////////////////////////////////////////////////////////
+// 
+//  HKLM\Security\SuRun keys
+// 
+//////////////////////////////////////////////////////////////////////////////
+
+#define PASSWKEY      SVCKEY _T("\\Cache")
+#define TIMESKEY      SVCKEY _T("\\Times")
+#define WHTLSTKEY(u)  CBigResStr(_T("%s\\%s"),SVCKEY,u)
+#define USERKEY(u)    CBigResStr(_T("%s\\%s\\Settings"),SVCKEY,u)
+
+//////////////////////////////////////////////////////////////////////////////
+// 
+//  Macros for all Settings
+// 
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
 //Settings for all users; saved to "HKLM\SECURITY\SuRun":
-extern bool g_BlurDesktop;      //blurred user desktop background on secure Desktop
-extern BYTE g_NoAskTimeOut;     //Minutes to wait until "Is that OK?" is asked again
-extern bool g_bSavePW;          //Save Passwords in Registry
+//////////////////////////////////////////////////////////////////////////////
+#define GetOption(s,d) GetRegInt(HKLM,SVCKEY,s,d)
+#define SetOption(s,v) SetRegInt(HKLM,SVCKEY,s,v)
 
+//blurred user desktop background on secure Desktop
+#define GetBlurDesk       (GetOption(_T("BlurDesktop"),1)!=0)
+#define SetBlurDesk(b)    SetOption(_T("BlurDesktop"),b)
+
+//Minutes to wait until "Is that OK?" is asked again
+#define GetPwTimeOut      min(60,max(0,(int)GetOption(_T("AskTimeOut"),0)))
+#define SetPwTimeOut(t)   min(60,max(0,(int)SetOption(_T("AskTimeOut"),t)))
+
+#define GetSavePW         (GetOption(_T("SavePasswords"),0)!=0)
+#define SetSavePW(b)      SetOption(_T("SavePasswords"),b); \
+                          if (!b) \
+                          { \
+                            DelRegKey(HKLM,PASSWKEY); \
+                            DelRegKey(HKLM,TIMESKEY); \
+                          }
+
+//////////////////////////////////////////////////////////////////////////////
 //Settings for every user; saved to "HKLM\SECURITY\SuRun\<ComputerName>\<UserName>":
-extern bool g_bAdminOnlySetup;  //Only real Admins may run Setup
-extern bool g_bRestricApps;     //SuRunner may only run predefined Applications
+//////////////////////////////////////////////////////////////////////////////
+#define GetUsrSetting(u,s,d)  GetRegInt(HKLM,USERKEY(u),s,d)
+#define SetUsrSetting(u,s,v)  SetRegInt(HKLM,USERKEY(u),s,v)
 
+//SuRunner is not allowed to run Setup
+#define GetNoRunSetup(u)      (GetUsrSetting(u,_T("AdminOnlySetup"),0)!=0)
+#define SetNoRunSetup(u,b)    SetUsrSetting(u,_T("AdminOnlySetup"),b)
+//SuRunner may only run predefined Applications
+#define GetRestrictApps(u)    (GetUsrSetting(u,_T("RestrictApps"),0)!=0)
+#define SetRestrictApps(u,b)  SetUsrSetting(u,_T("RestrictApps"),b)
+
+//////////////////////////////////////////////////////////////////////////////
 //Shell Extension Settings; stored in: HKCR\\CLSID\\sGUID
-extern bool g_bControlAsAdmin;  //"Control Panel As Admin" on Desktop Menu
-extern bool g_bCmdHereAsAdmin;  //"Cmd here As Admin" on Folder Menu
-extern bool g_bExpHereAsAdmin;  //"Explorer here As Admin" on Folder Menu
-extern bool g_bRestartAsAdmin;  //"Restart As Admin" in System-Menu
-extern bool g_bStartAsAdmin;    //"Start As Admin" in System-Menu
+//////////////////////////////////////////////////////////////////////////////
+#define GetShExtSetting(s,d)  GetRegInt(HKCR,L"CLSID\\" sGUID,s,d)
+#define SetShExtSetting(s,v)  SetRegInt(HKCR,L"CLSID\\" sGUID,s,v)
+
+//the following settings are stored in: HKCR\\CLSID\\sGUID
+#define ControlAsAdmin  L"ControlAsAdmin"  //"Control Panel As Admin" on Desktop Menu
+#define CmdHereAsAdmin  L"CmdHereAsAdmin"  //"Cmd here As Admin" on Folder Menu
+#define ExpHereAsAdmin  L"ExpHereAsAdmin"  //"Explorer here As Admin" on Folder Menu
+#define RestartAsAdmin  L"RestartAsAdmin"  //"Restart As Admin" in System-Menu
+#define StartAsAdmin    L"StartAsAdmin"    //"Start As Admin" in System-Menu
+
+//"Control Panel As Admin" on Desktop Menu
+#define GetCtrlAsAdmin        (GetShExtSetting(ControlAsAdmin,1)!=0)
+#define SetCtrlAsAdmin(b)     SetShExtSetting(ControlAsAdmin,b)
+//"Cmd here As Admin" on Folder Menu
+#define GetCmdAsAdmin         (GetShExtSetting(CmdHereAsAdmin,1)!=0)
+#define SetCmdAsAdmin(b)      SetShExtSetting(CmdHereAsAdmin,b)
+//"Explorer here As Admin" on Folder Menu
+#define GetExpAsAdmin         (GetShExtSetting(ExpHereAsAdmin,1)!=0)
+#define SetExpAsAdmin(b)      SetShExtSetting(ExpHereAsAdmin,b)
+//"Restart As Admin" in System-Menu
+#define GetRestartAsAdmin     (GetShExtSetting(RestartAsAdmin,1)!=0)
+#define SetRestartAsAdmin(b)  SetShExtSetting(RestartAsAdmin,b)
+//"Start As Admin" in System-Menu
+#define GetStartAsAdmin       (GetShExtSetting(StartAsAdmin,1)!=0)
+#define SetStartAsAdmin(b)    SetShExtSetting(StartAsAdmin,b)
 
 //////////////////////////////////////////////////////////////////////////////
-// 
-//  LoadSettings
-// 
+//  Windows Policy Stuff
 //////////////////////////////////////////////////////////////////////////////
-void LoadSettings(LPTSTR UserName);
+
+#define IsOwnerAdminGrp     (GetRegInt(HKLM,\
+                              _T("SYSTEM\\CurrentControlSet\\Control\\Lsa"),\
+                              _T("nodefaultadminowner"),1)==0)
+
+#define SetOwnerAdminGrp(b) SetRegInt(HKLM,\
+                              _T("SYSTEM\\CurrentControlSet\\Control\\Lsa"),\
+                              _T("nodefaultadminowner"),(b)==0)
+
+#define IsWinUpd4All      GetRegInt(HKLM,\
+                            _T("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate"),\
+                            _T("ElevateNonAdmins"),0)
+
+#define SetWinUpd4All(b)  SetRegInt(HKLM,\
+                            _T("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate"),\
+                            _T("ElevateNonAdmins"),b)
+
+#define IsWinUpdBoot      GetRegInt(HKLM,\
+                            _T("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU"),\
+                            _T("NoAutoRebootWithLoggedOnUsers"),0)
+
+#define SetWinUpdBoot(b)  SetRegInt(HKLM,\
+                            _T("SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU"),\
+                            _T("NoAutoRebootWithLoggedOnUsers"),b)
+
+#define CanSetEnergy  HasRegistryKeyAccess(_T("MACHINE\\Software\\Microsoft\\")\
+                    _T("Windows\\CurrentVersion\\Controls Folder\\PowerCfg"),SURUNNERSGROUP)
+
+#define SetEnergy(b)  SetRegistryTreeAccess(_T("MACHINE\\Software\\Microsoft\\")\
+                    _T("Windows\\CurrentVersion\\Controls Folder\\PowerCfg"),SURUNNERSGROUP,b)
 
 //////////////////////////////////////////////////////////////////////////////
 // 
@@ -53,10 +145,7 @@ void SavePassword(LPTSTR UserName,LPTSTR Password);
 // 
 //////////////////////////////////////////////////////////////////////////////
 
-//FILETIME(100ns) to minutes multiplier
-#define ft2min  (__int64)(10/*1µs*/*1000/*1ms*/*1000/*1s*/*60/*1min*/)
-
-__int64 GetLastRunTime(LPTSTR UserName);
+BOOL PasswordExpired(LPTSTR UserName);
 void UpdLastRunTime(LPTSTR UserName);
 
 //////////////////////////////////////////////////////////////////////////////
