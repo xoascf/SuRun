c86713c970318c3c6e1eab2b7413e27cdd1beb8d
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-11-30 20:41:58 +0000

    User: Kay          Date: 30.11.07   Time: 23:43 (vss version: 9, vss user: Kay)

--- SuRun/Setup.h
+++ SuRun/Setup.h
@@ -56,6 +56,10 @@
                             DelRegKey(HKLM,TIMESKEY); \
                           }
 
+//All Users may use SuRun
+#define GetAllowAll       (GetOption(_T("AllowAllUsers"),0)!=0)
+#define SetAllowAll(b)    SetOption(_T("AllowAllUsers"),b)
+
 //////////////////////////////////////////////////////////////////////////////
 //Settings for every user; saved to "HKLM\SECURITY\SuRun\<ComputerName>\<UserName>":
 //////////////////////////////////////////////////////////////////////////////
