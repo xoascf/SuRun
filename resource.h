70b5a40570d124232a6fe950b5db74d650e46d95
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-12-02 10:14:00 +0000

    User: Kay          Date:  2.12.07   Time: 14:51 (vss version: 33, vss user: Kay)

--- resource.h
+++ resource.h
@@ -110,7 +110,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        114
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1044
+#define _APS_NEXT_CONTROL_VALUE         1045
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
