a1a1b2e742382b78abcc420e13f4ec87d71ef765
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2007-12-19 09:11:21 +0000

    User: Kay          Date: 19.12.07   Time: 10:48 (vss version: 15, vss user: Kay)

--- UserGroups.h
+++ UserGroups.h
@@ -52,7 +52,7 @@ BOOL IsInGroup(DWORD Rid,LPCWSTR DomainAndName);
 BOOL IsInSuRunners(LPCWSTR DomainAndName);
 
 // is User "DomainAndName" the built in administrator? (can't be degraded)
-BOOL IsBuiltInAdmin(LPCWSTR DomainAndName);
+//BOOL IsBuiltInAdmin(LPCWSTR DomainAndName);
 
 //  BeOrBecomeSuRunner: check, if User is SuRunner if not, try to join him
 BOOL BeOrBecomeSuRunner(LPCTSTR UserName,BOOL bVerifyAdmin=TRUE,HWND hwnd=0);
