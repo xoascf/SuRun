90bc655bd1242bd17d1660be4779b987add9b50e
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-11 22:13:47 +0000

    User: Kay          Date: 11.02.08   Time: 23:16 (vss version: 2, vss user: Kay)

--- SuRun/SuRunExt/IATHook.cpp
+++ SuRun/SuRunExt/IATHook.cpp
@@ -13,81 +13,70 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include <windows.h>
+#include <Psapi.h>
 #include "SysMenuHook.h"
 
-DWORD WINAPI HookIAT(HMODULE hMod,LPCTSTR ModName,PROC origFunc, PROC newFunc)
+#pragma comment(lib,"PSAPI.lib")
+
+DWORD WINAPI HookIAT(LPCSTR DllName,PROC origFunc, PROC newFunc)
 {
-  PIMAGE_DOS_HEADER pDosH;
-  PIMAGE_NT_HEADERS pNTH;
-  PIMAGE_IMPORT_DESCRIPTOR pImportDesc;
-  PIMAGE_EXPORT_DIRECTORY pExportDir;
-  PIMAGE_THUNK_DATA pThunk;
-  PIMAGE_IMPORT_BY_NAME pImportName;
-  
-  if(!newFunc || !hMod || hMod == g_hInst)
-    return 0;
-  
   // Verify that the newFunc is valid
-  if (IsBadCodePtr(newFunc))
+  if((!newFunc)||(IsBadCodePtr(newFunc)))
     return 0;
-  
-  // Get DOS Header
-  pDosH = (PIMAGE_DOS_HEADER) hMod;
-  
-  // Verify that the PE is valid by checking e_magic's value and DOS Header size
-  if(IsBadReadPtr(pDosH, sizeof(IMAGE_DOS_HEADER)))
+  HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS,TRUE,GetCurrentProcessId());
+  if (!hProc)
     return 0;
-  
-  if(pDosH->e_magic != IMAGE_DOS_SIGNATURE)
-    return 0;
-  
-  // Find the NT Header by using the offset of e_lfanew value from hMod
-  pNTH = (PIMAGE_NT_HEADERS) ((DWORD) pDosH + (DWORD) pDosH->e_lfanew);
-  
-  // Verify that the NT Header is correct
-  if(IsBadReadPtr(pNTH, sizeof(IMAGE_NT_HEADERS)))
-    return 0;
-  
-  if(pNTH->Signature != IMAGE_NT_SIGNATURE)
-    return 0;
-  
-  // iat patching
-  pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR) ((DWORD) pDosH +
-    (DWORD) (pNTH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress));
-  
-  if(pImportDesc == (PIMAGE_IMPORT_DESCRIPTOR) pNTH)
-    return 0;
-  
-  while(pImportDesc->Name)
+  DWORD Siz=0;
+  EnumProcessModules(hProc,0,0,&Siz);
+  HMODULE* hModues=(HMODULE*)malloc(Siz);
+  if(!hModues)
+    return CloseHandle(hProc),0;
+  EnumProcessModules(hProc,hModues,Siz,&Siz);
+  for (HMODULE* hMod=hModues;hMod<hModues+Siz;hMod++) if(*hMod != g_hInst)
   {
-    // pImportDesc->Name gives the name of the module, so we can find "user32.dll"
-    char *name = (char *) ((DWORD) pDosH + (DWORD) (pImportDesc->Name));
-    // stricmp returns 0 if strings are equal, case insensitive
-    if(_stricmp(name, ModName) == 0)
+    // Get DOS Header
+    PIMAGE_DOS_HEADER pDosH = (PIMAGE_DOS_HEADER) *hMod;
+    // Verify that the PE is valid by checking e_magic's value and DOS Header size
+    if(IsBadReadPtr(pDosH, sizeof(IMAGE_DOS_HEADER))
+      || (pDosH->e_magic != IMAGE_DOS_SIGNATURE))
+      continue;
+    // Find the NT Header by using the offset of e_lfanew value from hMod
+    PIMAGE_NT_HEADERS pNTH = (PIMAGE_NT_HEADERS) ((DWORD) pDosH + (DWORD) pDosH->e_lfanew);
+    // Verify that the NT Header is correct
+    if(IsBadReadPtr(pNTH, sizeof(IMAGE_NT_HEADERS))
+      ||(pNTH->Signature != IMAGE_NT_SIGNATURE))
+      continue;
+    // iat patching
+    PIMAGE_IMPORT_DESCRIPTOR pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR) ((DWORD) pDosH +
+      (DWORD) (pNTH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress));
+    if(pImportDesc == (PIMAGE_IMPORT_DESCRIPTOR) pNTH)
+      continue;
+    while(pImportDesc->Name)
     {
-      pThunk = (PIMAGE_THUNK_DATA)((DWORD) pDosH + (DWORD) pImportDesc->FirstThunk);
-      while(pThunk->u1.Function)
+      // pImportDesc->Name gives the name of the module, so we can find "user32.dll"
+      char *name = (char *) ((DWORD) pDosH + (DWORD) (pImportDesc->Name));
+      // stricmp returns 0 if strings are equal, case insensitive
+      if(_stricmp(name, DllName) == 0)
       {
-        // get the pointer of the imported function and see if it matches up with the original
-        if((DWORD) pThunk->u1.Function == (DWORD) origFunc)
+        PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)((DWORD) pDosH + (DWORD) pImportDesc->FirstThunk);
+        while(pThunk->u1.Function)
         {
-          MEMORY_BASIC_INFORMATION mbi;
-          DWORD oldProt;
-          VirtualQuery(&pThunk->u1.Function, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
-          VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_READWRITE, &oldProt);
-          pThunk->u1.Function = (DWORD) newFunc;
-          VirtualProtect(mbi.BaseAddress, mbi.RegionSize, oldProt, &oldProt);
-          break;
-        }
-        else
-        {
+          // get the pointer of the imported function and see if it matches up with the original
+          if((DWORD) pThunk->u1.Function == (DWORD) origFunc)
+          {
+            MEMORY_BASIC_INFORMATION mbi;
+            DWORD oldProt;
+            VirtualQuery(&pThunk->u1.Function, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
+            VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_READWRITE, &oldProt);
+            pThunk->u1.Function = (DWORD) newFunc;
+            VirtualProtect(mbi.BaseAddress, mbi.RegionSize, oldProt, &oldProt);
+            break;
+          }
           ++pThunk;
-        }      
+        }
       }
+      ++pImportDesc;
     }
-    
-    ++pImportDesc;
   }
-  
-  return 0;      
+  return free(hModues),CloseHandle(hProc),0;      
 }
