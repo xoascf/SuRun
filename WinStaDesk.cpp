51adf50d9f1327b5fe2860b08d600219f3835b15
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-14 07:31:01 +0000

    User: Kay          Date: 14.02.08   Time:  9:32 (vss version: 15, vss user: Kay)

--- WinStaDesk.cpp
+++ WinStaDesk.cpp
@@ -64,6 +64,156 @@ BOOL GetDesktopName(LPTSTR DeskName,DWORD ccDeskName)
 #define DESKTOP_ALL_ACCESS 0x00001FF
 #endif
 
+//////////////////////////////////////////////////////////////////////////////
+//
+// The following has evolved taken from Keith Browns cmdasuser
+//
+// http://www.develop.com/kbrown
+//
+//////////////////////////////////////////////////////////////////////////////
+
+// for some reason, winuser.h doesn't define these useful aliases
+#ifndef WINSTA_ALL_ACCESS
+#define WINSTA_ALL_ACCESS 0x0000037F
+#endif
+
+#ifndef DESKTOP_ALL_ACCESS
+#define DESKTOP_ALL_ACCESS 0x00001FF
+#endif
+
+void SetProcWinStaDesk(LPCTSTR WinSta,LPCTSTR Desk)
+{
+	HWINSTA hws = OpenWindowStation(WinSta,0,MAXIMUM_ALLOWED);
+	if (!hws)
+  {
+    DBGTrace2("OpenWindowStation(%s) failed: %s",WinSta,GetLastErrorNameStatic());
+  }else
+  {
+    SetProcessWindowStation(hws);
+    if (!CloseWindowStation(hws))
+      DBGTrace1("CloseWindowStation failed: %s",GetLastErrorNameStatic());
+    HDESK hd = OpenDesktop(Desk,0,0,MAXIMUM_ALLOWED);
+    if (!hd)
+    {
+      DBGTrace2("OpenDesktop(%s) failed: %s",Desk,GetLastErrorNameStatic());
+    }else
+    {
+      if (!SetThreadDesktop(hd))
+        DBGTrace1("SetThreadDesktop failed: %s",GetLastErrorNameStatic());
+      if (!CloseDesktop(hd))
+        DBGTrace1("CloseDesktop failed: %s",GetLastErrorNameStatic());
+    }
+  }
+}
+
+void SetAccessToWinDesk(HANDLE htok,LPCTSTR WinSta,LPCTSTR Desk,BOOL bGrant)
+{
+	BYTE tgs[4096];
+	DWORD cbtgs = sizeof tgs;
+	if (!GetTokenInformation(htok,TokenGroups,tgs,cbtgs,&cbtgs))
+  {
+    DBGTrace1("GetTokenInformation failed: %s",GetLastErrorNameStatic());
+    return;
+  }
+	const TOKEN_GROUPS* ptgs = (TOKEN_GROUPS*)(tgs);
+	const SID_AND_ATTRIBUTES* it = ptgs->Groups;
+	const SID_AND_ATTRIBUTES* end = it + ptgs->GroupCount;
+	while (end!=it)
+	{
+		if ((it->Attributes & SE_GROUP_LOGON_ID)==SE_GROUP_LOGON_ID)
+			break;
+		++it;
+	}
+	if (end==it)
+  {
+    DBGTrace("UNEXPECTED: No Logon SID in TokenGroups");
+    return;
+  }
+	void* psidLogonSession = it->Sid;
+	HWINSTA hws = OpenWindowStation(WinSta,0,MAXIMUM_ALLOWED);
+	if (!hws)
+  {
+    DBGTrace2("OpenWindowStation(%s) failed: %s",WinSta,GetLastErrorNameStatic());
+  }else
+	{
+    PSECURITY_DESCRIPTOR pSD = NULL;
+		PACL pOldDACL=NULL, pNewDACL=NULL;
+		// get the existing DACL, with enough extra space for adding a couple more aces
+    GetSecurityInfo(hws,SE_WINDOW_OBJECT,DACL_SECURITY_INFORMATION,0,0,&pOldDACL,0,&pSD);
+    // Initialize an EXPLICIT_ACCESS structure for an ACE.
+    EXPLICIT_ACCESS ea[2]={0};
+    // grant the logon session all access to winsta0
+    ea[1].grfAccessPermissions = WINSTA_ALL_ACCESS;
+    ea[1].grfAccessMode = bGrant?SET_ACCESS:REVOKE_ACCESS;
+    ea[1].grfInheritance= NO_INHERITANCE;
+    ea[1].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+    ea[1].Trustee.TrusteeType = TRUSTEE_IS_USER;
+    ea[1].Trustee.ptstrName  = (LPTSTR)psidLogonSession;
+    // grant the logon session all access to any new desktops created in winsta0
+		// by adding an inherit-only ace
+    ea[0].grfAccessPermissions = DESKTOP_ALL_ACCESS;
+    ea[0].grfAccessMode = bGrant?SET_ACCESS:REVOKE_ACCESS;
+    ea[0].grfInheritance= SUB_CONTAINERS_AND_OBJECTS_INHERIT|INHERIT_ONLY;
+    ea[0].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+    ea[0].Trustee.TrusteeType = TRUSTEE_IS_USER;
+    ea[0].Trustee.ptstrName  = (LPTSTR)psidLogonSession;
+    // Create a new ACL that merges the new ACE into the existing DACL.
+    DWORD err=SetEntriesInAcl(2,ea,pOldDACL,&pNewDACL);
+    if (err!=ERROR_SUCCESS)
+    {
+      DBGTrace1("SetEntriesInAcl failed:%s",GetErrorNameStatic(err));
+    }else
+    {
+      // apply changes
+      err=SetSecurityInfo(hws,SE_WINDOW_OBJECT,DACL_SECURITY_INFORMATION,0,0,pNewDACL,0);
+      if ( err )
+        DBGTrace1("SetSecurityInfo failed %s",GetErrorNameStatic(err));
+    }
+    LocalFree((HLOCAL)pNewDACL); 
+		LocalFree((HLOCAL)pSD); 
+    //We need to set hws to our WindowsStation to make Desk accessible to us 
+    HWINSTA hwss=GetProcessWindowStation();
+    SetProcessWindowStation(hws);
+	  HDESK hd = OpenDesktop(Desk,0,0,MAXIMUM_ALLOWED);
+	  if (!hd)
+    {
+      DBGTrace2("OpenDesktop(%s) failed: %s",Desk,GetLastErrorNameStatic());
+    }else
+	  {
+      PSECURITY_DESCRIPTOR pSD = NULL;
+		  PACL pOldDACL=NULL, pNewDACL=NULL;
+		  // get the existing DACL, with enough extra space for adding a couple more aces
+      GetSecurityInfo(hd,SE_WINDOW_OBJECT,DACL_SECURITY_INFORMATION,NULL,NULL,&pOldDACL,NULL,&pSD);
+      // Initialize an EXPLICIT_ACCESS structure for an ACE.
+      EXPLICIT_ACCESS ea={0};
+      // grant the logon session all access to the default desktop
+      ea.grfAccessPermissions = DESKTOP_ALL_ACCESS;
+      ea.grfAccessMode = bGrant?SET_ACCESS:REVOKE_ACCESS;
+      ea.grfInheritance= NO_INHERITANCE;
+      ea.Trustee.TrusteeForm = TRUSTEE_IS_SID;
+      ea.Trustee.TrusteeType = TRUSTEE_IS_USER;
+      ea.Trustee.ptstrName  = (LPTSTR)psidLogonSession;
+      // Create a new ACL that merges the new ACE into the existing DACL.
+      DWORD err=SetEntriesInAcl(1,&ea,pOldDACL,&pNewDACL);
+      if (err!=ERROR_SUCCESS)
+      {
+        DBGTrace1("SetEntriesInAcl failed:%s",GetErrorNameStatic(err));
+      }else
+      {
+        // apply changes
+        err=SetSecurityInfo(hd,SE_WINDOW_OBJECT,DACL_SECURITY_INFORMATION,0,0,pNewDACL,0);
+        if ( err )
+          DBGTrace1("SetSecurityInfo failed %s",GetErrorNameStatic(err));
+      }
+      LocalFree((HLOCAL)pNewDACL); 
+		  LocalFree((HLOCAL)pSD); 
+      CloseDesktop(hd);
+	  }
+    SetProcessWindowStation(hwss);
+    CloseWindowStation(hws);
+	}
+}
+
 void GrantUserAccessToDesktop(HDESK hDesk)
 {
   PSID UserSID=0;
