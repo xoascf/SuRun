21a538d64fa302607b013ce28483e0160195b8d3
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-17 19:05:24 +0000

    User: Kay          Date: 17.02.08   Time: 20:05 (vss version: 62, vss user: Kay)

--- SuRun/LogonDlg.cpp
+++ SuRun/LogonDlg.cpp
@@ -64,7 +64,7 @@ HANDLE GetUserToken(LPCTSTR User,LPCTSTR Password)
   if (!LogonUser(un,dn,Password,LOGON32_LOGON_NETWORK,0,&hToken))
     if (GetLastError()==ERROR_PRIVILEGE_NOT_HELD)
       hToken=SSPLogonUser(dn,un,Password);
-  DBGTrace4("GetUserToken(%s,%s,%s):%s",un,dn,Password,hToken?_T("SUCCEEDED."):_T("FAILED!"));
+//  DBGTrace4("GetUserToken(%s,%s,%s):%s",un,dn,Password,hToken?_T("SUCCEEDED."):_T("FAILED!"));
   //Reset status of "use of empty passwords for network logon"
   if ((Password==NULL) || (*Password==NULL))
     AllowEmptyPW(bEmptyPWAllowed);
