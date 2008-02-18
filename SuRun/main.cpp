44b059b218e708cfdbf709c6d3e188e47804e924
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-18 20:18:23 +0000

    User: Kay          Date: 18.02.08   Time: 22:43 (vss version: 107, vss user: Kay)

--- SuRun/main.cpp
+++ SuRun/main.cpp
@@ -203,16 +203,6 @@ int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nCmdS
       Args=PathGetArgs(Args);
       g_RunData.RetPtr=wcstoul(Args,0,16);
       Args=PathGetArgs(Args);
-    }else if (!_wcsicmp(c,L"/NEWDEV"))
-    {
-      g_RunData.bShlExHook=2;
-      //New Device
-      GetSystemDirectory(g_RunData.cmdLine,4096);
-      PathAppend(g_RunData.cmdLine,L"rundll32.exe");
-      _tcscat(g_RunData.cmdLine,L" ");
-      _tcscat(g_RunData.cmdLine,PathGetArgs(Args));
-      bRunSetup=TRUE;
-      break;
     }else if (!_wcsicmp(c,L"/KILL"))
     {
       g_RunData.KillPID=wcstol(Args,0,10);
