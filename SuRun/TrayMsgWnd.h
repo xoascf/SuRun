0a0e603004e0eb6f16febc0f74997ec198896e7a
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-21 19:31:38 +0000

    User: Kay          Date: 21.02.08   Time: 22:49 (vss version: 2, vss user: Kay)

--- SuRun/TrayMsgWnd.h
+++ SuRun/TrayMsgWnd.h
@@ -1,18 +1,15 @@
-#if !defined(AFX_TRAYMSGWND_H__0894CDC8_FC2A_4A01_B8A7_F5F3FEB75D8C__INCLUDED_)
-#define AFX_TRAYMSGWND_H__0894CDC8_FC2A_4A01_B8A7_F5F3FEB75D8C__INCLUDED_
-
-#pragma once
-// TrayMsgWnd.h : header file
+//////////////////////////////////////////////////////////////////////////////
 //
-void TrayMsg(LPTSTR DlgTitle,int nID,...);
-void TrayMsg(LPTSTR DlgTitle,CString s,...);
-
+// This source code is part of SuRun
+//
+// Some sources in this project evolved from Microsoft sample code, some from 
+// other free sources. The Application icons are from Foood's "iCandy" icon 
+// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
+// Service Pack 2 (xpsp2res.dll) 
+// 
+// Feel free to use the SuRun sources for your liking.
+// 
+//                                (c) Kay Bruns (http://kay-bruns.de), 2007,08
+//////////////////////////////////////////////////////////////////////////////
+#pragma once
 void TrayMsgWnd(LPCTSTR DlgTitle,LPCTSTR Message);
-void TrayMsgWnd(void* Buf);
-
-void TrayMsgClear();
-
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_TRAYMSGWND_H__0894CDC8_FC2A_4A01_B8A7_F5F3FEB75D8C__INCLUDED_)
