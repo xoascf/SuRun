50fd164589fb96f5bd56204e184ae27347de1d0f
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-02-26 14:34:16 +0000

    User: Kay          Date: 26.02.08   Time: 15:39 (vss version: 124, vss user: Kay)

--- main.cpp
+++ main.cpp
@@ -154,6 +154,7 @@ int Run()
 //////////////////////////////////////////////////////////////////////////////
 int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nCmdShow)
 {
+  HandleServiceStuff();
   //After the User presses OK, the service starts a clean SuRun exe with the 
   //Clients user token, it fills g_RunData and g_RunPwd
   //We must Do this for two reasons:
