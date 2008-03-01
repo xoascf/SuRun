217585f57bb3a052ee031379a70fa70b01ef5610
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-03-01 21:13:06 +0000

    User: Kay          Date:  1.03.08   Time: 23:13 (vss version: 11, vss user: Kay)

--- SuRunExt/SysMenuHook.h
+++ SuRunExt/SysMenuHook.h
@@ -3,9 +3,8 @@
 // This source code is part of SuRun
 //
 // Some sources in this project evolved from Microsoft sample code, some from 
-// other free sources. The Application icons are from Foood's "iCandy" icon 
-// set (http://www.iconaholic.com). the Shield Icons are taken from Windows XP 
-// Service Pack 2 (xpsp2res.dll) 
+// other free sources. The Shield Icons are taken from Windows XP Service Pack 
+// 2 (xpsp2res.dll) 
 // 
 // Feel free to use the SuRun sources for your liking.
 // 
