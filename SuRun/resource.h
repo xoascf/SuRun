eab2ac62a894179df93076eb18f8afaaddd93fa0
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-04-09 18:58:45 +0000

    User: Kay          Date:  9.04.08   Time: 20:27 (vss version: 71, vss user: Kay)

--- SuRun/resource.h
+++ SuRun/resource.h
@@ -204,9 +204,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1076
+#define _APS_NEXT_CONTROL_VALUE         1077
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
