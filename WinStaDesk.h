007ee5ed8076adc4f860f45645f1fb160cb03a0f
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-04-25 19:46:26 +0000

    User: Kay          Date: 25.04.08   Time: 20:50 (vss version: 18, vss user: Kay)

--- WinStaDesk.h
+++ WinStaDesk.h
@@ -31,4 +31,5 @@ void SetProcWinStaDesk(LPCTSTR WinSta,LPCTSTR Desk);
 void SetAccessToWinDesk(HANDLE htok,LPCTSTR WinSta,LPCTSTR Desk,BOOL bGrant);
 
 bool CreateSafeDesktop(LPTSTR WinSta,bool BlurDesk,bool bFade);
+HWND GetSafeDeskWnd();
 void DeleteSafeDesktop(bool bFade);
