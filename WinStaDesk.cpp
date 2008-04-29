f1f66c4f38b2ea26a228eae36f31bd1373a63faf
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-04-29 12:18:40 +0000

    (vss version: 36, vss user: Kay)

--- WinStaDesk.cpp
+++ WinStaDesk.cpp
@@ -18,7 +18,6 @@
 #include <aclapi.h>
 #include <shlwapi.h>
 #include <userenv.h>
-#include <strsafe.h>
 #include "WinStaDesk.h"
 #include "ScreenSnap.h"
 #include "Setup.h"
@@ -569,15 +568,15 @@ bool CreateSafeDesktop(LPTSTR WinSta,LPCTSTR UserDesk,bool BlurDesk,bool bFade)
     TCHAR SuRunExe[4096];
     GetSystemWindowsDirectory(SuRunExe,4096);
     PathAppend(SuRunExe,L"SuRun.exe /WATCHDOG");
-    StringCchCat(SuRunExe,4096,L" ");
-    StringCchCat(SuRunExe,4096,DeskName);
-    StringCchCat(SuRunExe,4096,L" ");
-    StringCchCat(SuRunExe,4096,UserDesk);
+    _tcscat(SuRunExe,L" ");
+    _tcscat(SuRunExe,DeskName);
+    _tcscat(SuRunExe,L" ");
+    _tcscat(SuRunExe,UserDesk);
     PROCESS_INFORMATION pi={0};
     STARTUPINFO si={0};
     si.cb	= sizeof(si);
     TCHAR WinstaDesk[MAX_PATH];
-    StringCchPrintf(WinstaDesk,MAX_PATH,_T("%s\\%s"),WinSta,UserDesk);
+    _stprintf(WinstaDesk,_T("%s\\%s"),WinSta,UserDesk);
     si.lpDesktop = WinstaDesk;
     if (CreateProcess(NULL,(LPTSTR)SuRunExe,NULL,NULL,FALSE,NORMAL_PRIORITY_CLASS,NULL,NULL,&si,&pi))
     {
