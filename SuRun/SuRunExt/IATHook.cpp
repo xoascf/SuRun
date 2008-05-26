b8b8968ebe9097366c06f49eedafea1a681cfb14
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-05-26 09:35:45 +0000

    (vss version: 105, vss user: Kay)

--- SuRun/SuRunExt/IATHook.cpp
+++ SuRun/SuRunExt/IATHook.cpp
@@ -286,7 +286,7 @@ DWORD TestAutoSuRunW(LPCWSTR lpApp,LPWSTR lpCmd,LPCWSTR lpCurDir,
   static TCHAR CurDir[4096];
   zero(CurDir);
   if (lpCurDir && *lpCurDir)
-    StringCchCopy(CurDir,4096,lpCurDir);
+    _tcscpy(CurDir,lpCurDir);
   else
     GetCurrentDirectory(countof(CurDir),CurDir);
   WCHAR* parms=(lpCmd && wcslen(lpCmd))?lpCmd:0;
@@ -294,17 +294,17 @@ DWORD TestAutoSuRunW(LPCWSTR lpApp,LPWSTR lpCmd,LPCWSTR lpCurDir,
   zero(cmd);
   if(lpApp)
   {
-    StringCchCatW(cmd,4096,lpApp);
+    wcscat(cmd,lpApp);
     PathQuoteSpacesW(cmd);
     if (parms)
       //lpApplicationName and the first token of lpCommandLine are the same
       //we need to check this:
       parms=PathGetArgsW(lpCmd);
     if (parms)
-      StringCchCatW(cmd,4096,L" ");
+      wcscat(cmd,L" ");
   }
   if (parms)
-    StringCchCatW(cmd,4096,parms);
+    wcscat(cmd,parms);
   PROCESS_INFORMATION rpi={0};
   //ToDo: Directly write to service pipe!
   {
@@ -326,8 +326,8 @@ DWORD TestAutoSuRunW(LPCWSTR lpApp,LPWSTR lpCmd,LPCWSTR lpCurDir,
     if (_wcsnicmp(cmd,tmp,wcslen(cmd))==0)
       //Never start SuRun administrative
       return LeaveCriticalSection(&g_HookCs),RETVAL_SX_NOTINLIST;
-    StringCchPrintf(&cmd[wcslen(cmd)],4096-wcslen(cmd),
-      L" /QUIET /TESTAA %d %x %s",GetCurrentProcessId(),&rpi,tmp);
+    wsprintf(&cmd[wcslen(cmd)],L" /QUIET /TESTAA %d %x %s",
+      GetCurrentProcessId(),&rpi,tmp);
   }
   //CTimeLog l(L"IATHook TestAutoSuRun(%s)",lpCmd);
   static STARTUPINFOW si;
