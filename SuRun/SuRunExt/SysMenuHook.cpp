223625e8bd6a8e5b7c5258a31d18b02268afcd49
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-06-12 08:58:17 +0000

    (vss version: 74, vss user: Kay)

--- SuRun/SuRunExt/SysMenuHook.cpp
+++ SuRun/SuRunExt/SysMenuHook.cpp
@@ -19,7 +19,6 @@
 #include <tchar.h>
 #include <Psapi.h>
 #include <shlwapi.h>
-#include <strsafe.h>
 
 #include "SysMenuHook.h"
 #include "../DBGTrace.H"
@@ -124,7 +123,7 @@ LRESULT CALLBACK MenuProc(int nCode, WPARAM wParam, LPARAM lParam)
     GetSystemWindowsDirectory(cmd, 4096);
     PathAppend(cmd, _T("SuRun.exe"));
     PathQuoteSpaces(cmd);
-    StringCchCat(cmd,4096,_T(" "));
+    _tcscat(cmd,_T(" "));
 //    TCHAR PID[10];
 //    if (msg->wParam==WM_SYSMH0)
 //    {
@@ -133,11 +132,11 @@ LRESULT CALLBACK MenuProc(int nCode, WPARAM wParam, LPARAM lParam)
 //        &&(SafeMsgBox(0,CResStr(l_hInst,IDS_RESTARTSHELL,GetCommandLine()),
 //                      L"SuRun",MB_ICONQUESTION|MB_YESNO|MB_DEFBUTTON2)==IDNO))
 //        return CallNextHookEx(g_hookMenu, nCode, wParam, lParam);
-//      StringCchCat(cmd,4096,_T("/KILL "));
-//      StringCchCat(cmd,4096,_itot(GetCurrentProcessId(),PID,10));
-//      StringCchCat(cmd,4096,_T(" "));
+//      _tcscat(cmd,_T("/KILL "));
+//      _tcscat(cmd,_itot(GetCurrentProcessId(),PID,10));
+//      _tcscat(cmd,_T(" "));
 //    }
-    StringCchCat(cmd,4096,GetCommandLine());
+    _tcscat(cmd,GetCommandLine());
     if (CreateProcess(NULL,cmd,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi))
     {
       CloseHandle(pi.hProcess);
