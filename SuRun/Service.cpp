82fc8859ce7ddc730e3a79ed08b72341ad6d6767
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-07-04 14:31:12 +0000

    User: Kay          Date:  4.07.08   Time: 16:32 (vss version: 330, vss user: Kay)

--- SuRun/Service.cpp
+++ SuRun/Service.cpp
@@ -985,9 +985,9 @@ DWORD LSAStartAdminProcess()
       {
         CloseHandle(pi.hThread);
         CloseHandle(pi.hProcess);
-        //Store PID in registry so WinLogon-LogOff can kill the process
-        //(Enumprocesses does not work from WinLogon-LogOff)
-        SetRegInt(HKLM,PIDSKEY,CResStr(L"%d",pi.dwProcessId),pi.dwProcessId);
+//        //Store PID in registry so WinLogon-LogOff can kill the process
+//        //(Enumprocesses does not work from WinLogon-LogOff)
+//        SetRegInt(HKLM,PIDSKEY,CResStr(L"%d",pi.dwProcessId),pi.dwProcessId);
         RetVal=RETVAL_OK;
         //ShellExec-Hook: We must return the PID and TID to fake CreateProcess:
         if((g_RunData.RetPID)&&(g_RunData.RetPtr))
