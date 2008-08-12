629457a35dc8c3c54b682530f14bbf7c8303d982
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-08-12 10:06:08 +0000

    User: Kay          Date: 12.08.08   Time: 11:13 (vss version: 15, vss user: Kay)

--- SuRun/IsAdmin.cpp
+++ SuRun/IsAdmin.cpp
@@ -254,7 +254,7 @@ BOOL RunAsAdmin(LPCTSTR cmdline,int IDmsg)
   TCHAR User[UNLEN+GNLEN+2]={0};
   TCHAR Password[PWLEN+1]={0};
   GetProcessUserName(GetCurrentProcessId(),User);
-  BOOL bRet=LogonAdmin(User,Password,IDmsg) 
+  BOOL bRet=LogonAdmin(-1,User,Password,IDmsg) 
     && RunAs(cmdline,User,Password);
   zero(User);      //Clean sensitive Data
   zero(Password);  //Clean sensitive Data
