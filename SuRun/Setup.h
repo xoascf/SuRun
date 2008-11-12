0f25abfb7b6f352e49d4cc50e96346770ef2ad19
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-11-12 19:51:52 +0000

    User: Kay          Date: 12.11.08   Time: 23:12 (vss version: 88, vss user: Kay)

--- SuRun/Setup.h
+++ SuRun/Setup.h
@@ -365,5 +365,5 @@ void ReplaceSuRunWithRunAs(HKEY hKey=HKCR);
 
 BOOL RunSetup(DWORD SessionID,LPCTSTR User);
 
-void ImportSettings(LPCTSTR ini);
-void ExportSettings(LPCTSTR ini);
+void ImportSettings(LPTSTR ini);
+void ExportSettings(LPTSTR ini);
