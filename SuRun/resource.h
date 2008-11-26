de62e31479552b62ffc9a17cdf58a151bbb86443
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2008-11-26 08:06:12 +0000

    User: Kay          Date: 26.11.08   Time: 11:03 (vss version: 99, vss user: Kay)

--- SuRun/resource.h
+++ SuRun/resource.h
@@ -145,6 +145,7 @@
 #define IDI_LISTICON4                   205
 #define IDI_LISTICON6                   206
 #define IDI_LISTICON7                   207
+#define IDI_LISTICON8                   208
 #define IDC_USER                        1000
 #define IDC_DLGQUESTION                 1001
 #define IDC_PASSWORD                    1002
@@ -253,7 +254,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1102
 #define _APS_NEXT_SYMED_VALUE           102
