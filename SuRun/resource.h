e1a295309bcee98707f9d24d2bc80256ba2ab3b6
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-03-03 18:34:55 +0000

    User: Kay          Date:  3.03.09   Time: 20:43 (vss version: 105, vss user: Kay)

--- SuRun/resource.h
+++ SuRun/resource.h
@@ -233,6 +233,7 @@
 #define IDC_KEEPREGST                   1072
 #define IDC_TRAYBALLOON                 1073
 #define IDC_STOREPASS                   1074
+#define IDC_NOLOGONDESK                 1074
 #define IDC_WARNADMIN                   1075
 #define IDC_AUTOCANCEL                  1077
 #define IDC_HIDESURUN                   1078
