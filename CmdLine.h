f549898644529e0cf51b2e81bb79aa3e300c72db
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-03-04 19:11:11 +0000

    Benutzer: Kay          Datum: 19.09.07   Uhrzeit:  9:05 $/SuRun eingecheckt Kommentar:  (vss 3)

--- CmdLine.h
+++ CmdLine.h
@@ -23,6 +23,8 @@
 #include <SHLWAPI.H>
 #pragma comment(lib,"ShlWapi.lib")
 
+#pragma warning(disable: 4996)
+
 class CCmdLine
 {
 public:
@@ -31,7 +33,8 @@ public:
     m_CmdLine=_tcsdup(GetCommandLine());
     m_Argc=0;
     //Count command line args and remove spaces
-    for (LPTSTR p=m_CmdLine;p && *p;m_Argc++)
+    LPTSTR p=m_CmdLine;
+    for (;p && *p;m_Argc++)
     {
       p=PathGetArgs(p);
       PathRemoveBlanks(p);
@@ -40,7 +43,8 @@ public:
     m_Args=(LPTSTR*)malloc(sizeof(LPTSTR)*m_Argc);
     //set argv pointers
     p=m_CmdLine;
-    for (int arg=0;arg<m_Argc;arg++)
+    int arg=0;
+    for (;arg<m_Argc;arg++)
     {
       //separate argv strings with '\0':
       if (arg)
