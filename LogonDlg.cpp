b4e1a15fe13808e3dfcb18e611c998af4761bd2a
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-03-09 06:35:19 +0000

    User: Kay          Date:  9.03.09   Time:  9:56 (vss version: 119, vss user: Kay)

--- LogonDlg.cpp
+++ LogonDlg.cpp
@@ -383,21 +383,21 @@ INT_PTR CALLBACK DialogProc(HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam)
         SendDlgItemMessage(hwnd,IDC_HINT,WM_SETFONT,
           (WPARAM)CreateFont(-14,0,0,0,FW_NORMAL,0,0,0,0,0,0,0,0,_T("MS Shell Dlg")),1);
       SetDlgItemText(hwnd,IDC_DLGQUESTION,p->Msg);
-#ifndef SuRunEXT_EXPORTS
-#ifdef DoDBGTrace
-      {
-        static TCHAR c1[4096];
-        _tcscpy(&c1[_tcslen(c1)],L"{\r\n\tElapsed Time from \"SuRun.exe\" start:");
-        for (DWORD i=1;i<g_nTimes;i++)
-        {
-          _stprintf(&c1[_tcslen(c1)],L"\r\n\t%s\t %d ms",
-            g_RunTimeNames[i],g_RunTimes[i]-g_RunTimes[0]);
-        }
-        SetDlgItemText(hwnd,IDC_DLGQUESTION,CBigResStr(L"%s\r\n%s\r\n\tGUI visible\t\t %d ms\r\n}",
-          p->Msg,c1,timeGetTime()-g_RunTimes[0]));
-      }
-#endif DoDBGTrace
-#endif SuRunEXT_EXPORTS
+//#ifndef SuRunEXT_EXPORTS
+//#ifdef DoDBGTrace
+//      {
+//        static TCHAR c1[4096];
+//        _tcscpy(&c1[_tcslen(c1)],L"{\r\n\tElapsed Time from \"SuRun.exe\" start:");
+//        for (DWORD i=1;i<g_nTimes;i++)
+//        {
+//          _stprintf(&c1[_tcslen(c1)],L"\r\n\t%s\t %d ms",
+//            g_RunTimeNames[i],g_RunTimes[i]-g_RunTimes[0]);
+//        }
+//        SetDlgItemText(hwnd,IDC_DLGQUESTION,CBigResStr(L"%s\r\n%s\r\n\tGUI visible\t\t %d ms\r\n}",
+//          p->Msg,c1,timeGetTime()-g_RunTimes[0]));
+//      }
+//#endif DoDBGTrace
+//#endif SuRunEXT_EXPORTS
       
       SetDlgItemText(hwnd,IDC_PASSWORD,p->Password);
       SendDlgItemMessage(hwnd,IDC_PASSWORD,EM_SETPASSWORDCHAR,'*',0);
