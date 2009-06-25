35bcdd25abb182a89870076e90896d65ab533519
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-06-25 06:39:43 +0000

    User: Kay          Date: 25.06.09   Time:  8:40 (vss version: 228, vss user: Kay)

--- SuRunExt/SuRunext.cpp
+++ SuRunExt/SuRunext.cpp
@@ -963,15 +963,15 @@ BOOL APIENTRY DllMain( HINSTANCE hInstDLL,DWORD dwReason,LPVOID lpReserved)
 #ifdef DoDBGTrace
     if(IsInBlackList(fMod))
       DBGTrace1("%s is blacklisted! No Hook!",fMod);
-//    DBGTrace5("Attach(hInst=%x) %d:%s[%s], NOAdmin, SetHook=%d",
-//      hInstDLL,PID,fMod,GetCommandLine(),l_bSetHook);
+    DBGTrace5("Attach(hInst=%x) %d:%s[%s], NOAdmin, SetHook=%d",
+      hInstDLL,PID,fMod,GetCommandLine(),l_bSetHook);
 #endif DoDBGTrace
     if(l_bSetHook && GetUseIATHook)
       LoadHooks();
   }
 #ifdef DoDBGTrace
-//  else
-//    DBGTrace4("Attach(hInst=%x) %d:%s[%s], ADMIN",hInstDLL,PID,fMod,GetCommandLine());
+  else
+    DBGTrace4("Attach(hInst=%x) %d:%s[%s], ADMIN",hInstDLL,PID,fMod,GetCommandLine());
 #endif DoDBGTrace
   //DevInst
 //  if(!l_IsAdmin)
