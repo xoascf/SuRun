ab1af2275731545b7fd6bba105b6a77a73071fbd
Kay <Kay@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-07-09 16:28:26 +0000

    Benutzer: Kay          Datum: 29.06.08   Uhrzeit: 11:50 $/SuRun eingecheckt Kommentar:  (vss version: 2, vss user: )

--- BuildSuRun.cmd
+++ BuildSuRun.cmd
@@ -2,8 +2,8 @@
 set VC6Dir=E:\VStudio
 Set MSSDK=E:\MSTOOLS
 
-call %VC6Dir%\VC98\Bin\VCVARS32.BAT
 SETLOCAL
+call %VC6Dir%\VC98\Bin\VCVARS32.BAT
 call %MSSDK%\SetEnv.Cmd /X64 /RETAIL
 echo building SuRunX64
 %VC6Dir%\Common\MSDev98\Bin\MSDEV.EXE /useenv SuRun.dsw /MAKE "SuRun - Win32 x64 Unicode Release" /REBUILD /OUT %TMP%\SuRun64.log
@@ -12,6 +12,7 @@ type %TMP%\SuRun64.log
 del %TMP%\SuRun64.log 1>NUL 2>NUL
 
 SETLOCAL
+call %VC6Dir%\VC98\Bin\VCVARS32.BAT
 call %MSSDK%\SetEnv.Cmd /2000 /RETAIL
 echo building SuRun32
 %VC6Dir%\Common\MSDev98\Bin\MSDEV.EXE /useenv SuRun.dsw /MAKE "SuRun - Win32 Unicode Release" "SuRun - Win32 SuRun32 Unicode Release" "InstallSuRun - Win32 Release" /REBUILD /OUT %TMP%\SuRun32.log
