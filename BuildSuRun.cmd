5009be9ee874738beef069da20931546e0c7884e
kbruns <kbruns@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2009-09-09 10:34:30 +0000

    VC8 batch build support

--- BuildSuRun.cmd
+++ BuildSuRun.cmd
@@ -1,6 +1,8 @@
 @echo off
-set VC6Dir=E:\VStudio
+if NOT "%VS80COMNTOOLS%"=="" goto VC8compile
+rem compile using VC6
 Set MSSDK=E:\MSTOOLS
+SET VC6Dir=%MSDevDir%\..\..
 
 SETLOCAL
 call %VC6Dir%\VC98\Bin\VCVARS32.BAT
@@ -19,4 +21,16 @@ echo building SuRun32
 ENDLOCAL
 type %TMP%\SuRun32.log
 del %TMP%\SuRun32.log 1>NUL 2>NUL
+goto Done
+
+rem compile using VC8 (2005)
+:VC8compile
+SETLOCAL
+call "%VS80COMNTOOLS%vsvars32.bat"
+"%DevEnvDir%\devenv" /rebuild "x64 Unicode Release|x64" "SuRun.sln"
+"%DevEnvDir%\devenv" /rebuild "SuRun32 Unicode Release|Win32" "SuRun.sln"
+"%DevEnvDir%\devenv" /rebuild "Unicode Release|Win32" "SuRun.sln"
+ENDLOCAL
+
+:Done
 pause
