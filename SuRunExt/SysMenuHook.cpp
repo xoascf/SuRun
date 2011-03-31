2aca573de95e67fc683220df305a370466a63e6d
kbruns <kbruns@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2011-03-31 13:07:06 +0000

    fixed "Restart As Admin" for Captionless Windows

--- SuRunExt/SysMenuHook.cpp
+++ SuRunExt/SysMenuHook.cpp
@@ -55,6 +55,8 @@ extern HANDLE    l_InitThread;
 
 UINT        WM_SYSMH0   = -2;
 UINT        WM_SYSMH1   = -2;
+UINT        WM_SYSMH2   = -2;
+UINT        WM_SYSMH3   = -2;
 
 
 BOOL g_IsShell=-1;
@@ -68,25 +70,6 @@ BOOL IsShell()
   return g_IsShell;
 }
 
-//BOOL g_IsWin7Explorer=-1;
-//
-//BOOL IsWin7Explorer()
-//{
-//  if (g_IsWin7Explorer!=-1)
-//    return g_IsWin7Explorer;
-//  g_IsWin7Explorer=FALSE;
-//  if (!IsWin7)
-//    return FALSE;
-//  TCHAR app[MAX_PATH]={0};
-//  GetSystemWindowsDirectory(app,MAX_PATH);
-//  PathAppend(app,_T("explorer.exe"));
-//
-//  TCHAR cmd[MAX_PATH]={0};
-//  GetProcessFileName(cmd,MAX_PATH);
-//  g_IsWin7Explorer=_tcsicmp(cmd,app)==0;
-//  return g_IsWin7Explorer;
-//}
-
 UINT GetMenuItemType(HMENU m,int pos)
 {
   MENUITEMINFO mii={0};
@@ -99,13 +82,32 @@ UINT GetMenuItemType(HMENU m,int pos)
 int FindSCClose(HMENU m)
 {
   //return position of SC_CLOSE in menu.
-  //Insert Separator before SC_CLOSE if no separator precedes it
   int i;
-  for (i=0;i<GetMenuItemCount(m)&&(GetMenuItemID(m,i)!=SC_CLOSE);i++);
+  for (i=0;i<GetMenuItemCount(m)&&(GetMenuItemID(m,i)!=SC_CLOSE);i++)
+    ;
+  if(GetMenuItemID(m,i)!=SC_CLOSE)
+    return -1;
+  //Insert Separator before SC_CLOSE if no separator precedes it
   if(i && ((GetMenuItemType(m,i-1)&MFT_SEPARATOR)==0))
-    InsertMenu(m,i++,MF_SEPARATOR|MF_BYPOSITION,WM_SYSMH0,0);
+  {
+    if(InsertMenu(m,i,MF_SEPARATOR|MF_BYPOSITION,WM_SYSMH2,0))
+      i++;
+  }
   return i;
 }
+
+//#ifdef DoDBGTrace
+//void DBGOutMsg(LPCTSTR Name,UINT message,WPARAM wParam,LPARAM lParam,LRESULT lResult)
+//{
+//  TCHAR f[MAX_PATH];
+//  GetProcessFileName(f,MAX_PATH);
+//  PathStripPath(f);
+//  TRACEx(_T("%s SuRunExt %s (%s,0x%08X,0x%08X)==0x%08X"),
+//    f,Name,MsgName(message),wParam,lParam,lResult);
+//}
+//#else DoDBGTrace
+//#define DBGOutMsg(m)
+//#endif DoDBGTrace
 
 LRESULT CALLBACK ShellProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -117,56 +119,96 @@ LRESULT CALLBACK ShellProc(int nCode, WPARAM wParam, LPARAM lParam)
       WM_SYSMH0=RegisterWindowMessage(_T("SYSMH1_2C7B6088-5A77-4d48-BE43-30337DCA9A86"));
     if (WM_SYSMH1==-2)
       WM_SYSMH1=RegisterWindowMessage(_T("SYSMH2_2C7B6088-5A77-4d48-BE43-30337DCA9A86"));
-    #define wps ((CWPSTRUCT*)lParam)
-    #define hmenu (HMENU)wps->wParam
+    if (WM_SYSMH2==-2)
+      WM_SYSMH2=RegisterWindowMessage(_T("SYSMH3_2C7B6088-5A77-4d48-BE43-30337DCA9A86"));
+    if (WM_SYSMH3==-2)
+      WM_SYSMH3=RegisterWindowMessage(_T("SYSMH3_2C7B6088-5A77-4d48-BE43-30337DCA9A86"));
+    #define wps ((CWPRETSTRUCT*)lParam)
     switch(wps->message)
     {
-    case WM_MENUSELECT:
-      if((wps->lParam==NULL)&&(HIWORD(wps->wParam)==0xFFFF))
+    case WM_UNINITMENUPOPUP:
+      #define hmenu (HMENU)wps->wParam
+      if(IsMenu(hmenu))
       {
-        HMENU m=GetSystemMenu(wps->hwnd,FALSE);
         //Two menu items and two separators can be in Sysmenu, remove them:
-        RemoveMenu(m,WM_SYSMH0,MF_BYCOMMAND);
-        RemoveMenu(m,WM_SYSMH0,MF_BYCOMMAND);
-        RemoveMenu(m,WM_SYSMH1,MF_BYCOMMAND);
-        RemoveMenu(m,WM_SYSMH1,MF_BYCOMMAND);
+        RemoveMenu(hmenu,WM_SYSMH2,MF_BYCOMMAND);
+        RemoveMenu(hmenu,WM_SYSMH0,MF_BYCOMMAND);
+        RemoveMenu(hmenu,WM_SYSMH1,MF_BYCOMMAND);
+        RemoveMenu(hmenu,WM_SYSMH3,MF_BYCOMMAND);
       }
+      #undef hmenu
       break;
     //Load the System menu for the Window, if we don't, we'll get a default 
     //system menu on the first click.
     case WM_CREATE:
+      if (wps->lResult==-1)
+        break;
     case WM_INITDIALOG:
       GetSystemMenu((HWND)wps->hwnd,FALSE);
       break;
+    case WM_SYSCOMMAND:
+      if((wps->wParam==WM_SYSMH0)||(wps->wParam==WM_SYSMH1))
+      {
+        //DBGOutMsg(_T("ShellProc"),wps->message,wps->wParam,wps->lParam,wps->lResult);
+        STARTUPINFO si={0};
+        PROCESS_INFORMATION pi;
+        si.cb = sizeof(si);
+        TCHAR cmd[4096];
+        GetSystemWindowsDirectory(cmd, 4096);
+        PathAppend(cmd, _T("SuRun.exe"));
+        PathQuoteSpaces(cmd);
+        _tcscat(cmd,_T(" "));
+        if (wps->wParam==WM_SYSMH0)
+        {
+          TCHAR PID[10];
+          _tcscat(cmd,_T("/KILL "));
+          _tcscat(cmd,_itot(GetCurrentProcessId(),PID,10));
+          _tcscat(cmd,_T(" "));
+        }
+        _tcscat(cmd,GetCommandLine());
+        if (CreateProcess(NULL,cmd,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi))
+        {
+          CloseHandle(pi.hProcess);
+          CloseHandle(pi.hThread);
+        }else
+          SafeMsgBox(wps->hwnd,CResStr(l_hInst,IDS_FILENOTFOUND),0,MB_ICONSTOP);
+        //We processed the Message: Stop calling other hooks!
+        return 0;
+      }
     case WM_INITMENUPOPUP:
-      if ((HIWORD(wps->lParam)==TRUE) 
-        && IsMenu(hmenu) 
+      #define hmenu (HMENU)wps->wParam
+      if (/*(HIWORD(wps->lParam)==TRUE)*/
+        /*&&*/ IsMenu(hmenu) 
         && (GetSystemMenu((HWND)wps->hwnd,FALSE)==hmenu)
-        /*&& (!IsWin7Explorer())*/
         && (!l_IsAdmin)
         && (!GetHideFromUser(l_User)))
       {
-        int i=0;
+        int i=-1;
         if( GetRestartAsAdmin 
         && (!IsShell())
         && (GetMenuState(hmenu,WM_SYSMH0,MF_BYCOMMAND)==(UINT)-1))
         {
           i=FindSCClose(hmenu);
-          InsertMenu(hmenu,i++,MF_BYPOSITION,WM_SYSMH0,CResStr(l_hInst,IDS_MENURESTART));
+          if (i>=0)
+            if (InsertMenu(hmenu,i,MF_BYPOSITION,WM_SYSMH0,CResStr(l_hInst,IDS_MENURESTART)))
+              i++;
         }
         if( GetStartAsAdmin
         && (GetMenuState(hmenu,WM_SYSMH1,MF_BYCOMMAND)==(UINT)-1))
         {
-          if (i==0)
+          if (i<0)
             i=FindSCClose(hmenu);
-          InsertMenu(hmenu,i++,MF_BYPOSITION,WM_SYSMH1,CResStr(l_hInst,IDS_MENUSTART));
+          if (i>=0)
+            if (InsertMenu(hmenu,i,MF_BYPOSITION,WM_SYSMH1,CResStr(l_hInst,IDS_MENUSTART)))
+              i++;
         }
-        if (i)
-          InsertMenu(hmenu,i,MF_SEPARATOR|MF_BYPOSITION,WM_SYSMH1,0);
+        if (i>=0)
+          if(InsertMenu(hmenu,i,MF_SEPARATOR|MF_BYPOSITION,WM_SYSMH2,0))
+            i++;
       }
+      #undef hmenu
       break;
     }
-    #undef hmenu
     #undef wps
   }
   return CallNextHookEx(g_hookShell, nCode, wParam, lParam);
@@ -178,6 +220,7 @@ LRESULT CALLBACK MenuProc(int nCode, WPARAM wParam, LPARAM lParam)
   if ((nCode>=0)&&(msg->message==WM_SYSCOMMAND)&&(wParam==PM_REMOVE)
     &&((msg->wParam==WM_SYSMH0)||(msg->wParam==WM_SYSMH1)))
   {
+    //DBGOutMsg(_T("MenuProc"),msg->message,msg->wParam,msg->lParam,0);
     STARTUPINFO si={0};
     PROCESS_INFORMATION pi;
     si.cb = sizeof(si);
@@ -219,7 +262,7 @@ __declspec(dllexport) BOOL InstallSysMenuHook()
     DBGTrace2("InstallSysMenuHook failed: Still Hooked (%x,%x)",g_hookShell,g_hookMenu);
     return FALSE;
   }
-  g_hookShell=SetWindowsHookEx(WH_CALLWNDPROC,(HOOKPROC)ShellProc,l_hInst,0);
+  g_hookShell=SetWindowsHookEx(WH_CALLWNDPROCRET,(HOOKPROC)ShellProc,l_hInst,0);
   if (g_hookShell==NULL)
     DBGTrace1("SetWindowsHookEx(Shell) failed: %s",GetLastErrorNameStatic());
   g_hookMenu =SetWindowsHookEx(WH_GETMESSAGE ,(HOOKPROC)MenuProc ,l_hInst,0);
