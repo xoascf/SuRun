ad8caa3d1aec660c704a7e0e6b3b002162288ab7
kbruns <kbruns@62e1e6ac-3918-6641-9eff-6129dd8e5ad8>
2011-05-11 19:58:11 +0000

    ...

--- SuRun/resource.h
+++ SuRun/resource.h
@@ -273,7 +273,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1104
+#define _APS_NEXT_CONTROL_VALUE         1105
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
